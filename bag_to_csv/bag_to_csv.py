77c9de04587d956dfe3d2e6ad64b33e3a4221320
Samuele Sandrini <samuele.sandrini@polito.it>
2024-07-30 17:29:49 +0200

    Lint

--- bag_to_csv/bag_to_csv.py
+++ bag_to_csv/bag_to_csv.py
@@ -1,12 +1,29 @@
+# Copyright 2024 National Research Council STIIMA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import importlib
+import sys
+from pathlib import Path
+
+import pandas as pd
 import rclpy
+from ament_index_python.packages import (PackageNotFoundError,
+                                         get_package_share_directory)
 from rclpy.node import Node
 from rosbags.highlevel import AnyReader
-from rosbags.typesys import Stores, get_typestore, get_types_from_msg
-from pathlib import Path
-import sys
-import pandas as pd
-from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
-import importlib
+from rosbags.typesys import Stores, get_types_from_msg, get_typestore
+
 
 class BagToCsvNode(Node):
     def __init__(self):
@@ -17,10 +34,10 @@ class BagToCsvNode(Node):
         self.declare_parameter('packages_to_register', [''])
         self.declare_parameter('extractors_to_import', [''])
 
-        bag_file_path = self.get_parameter('bag_file_path').get_parameter_value().string_value
-        csv_file_path = self.get_parameter('output_csv_path').get_parameter_value().string_value
-        packages_to_register = self.get_parameter('packages_to_register').get_parameter_value().string_array_value
-        extractors_to_import = self.get_parameter('extractors_to_import').get_parameter_value().string_array_value
+        bag_file_path = self._get_param('bag_file_path').string_value
+        csv_file_path = self._get_param('output_csv_path').string_value
+        packages_to_register = self._get_param('packages_to_register').string_array_value
+        extractors_to_import = self._get_param('extractors_to_import').string_array_value
 
         if not bag_file_path or not csv_file_path:
             self.get_logger().error('Both bag_file_path and csv_file_path parameters are required.')
@@ -36,20 +53,20 @@ class BagToCsvNode(Node):
         self.info_extractors = {}
         self.topics = []
         for extractor in extractors_to_import:
-            self.declare_parameter(f"{extractor}.package_name", '')
-            self.declare_parameter(f"{extractor}.module_name", '')
-            self.declare_parameter(f"{extractor}.class_name", '')
-            self.declare_parameter(f"{extractor}.topics", [''])
+            self.declare_parameter(f'{extractor}.package_name', '')
+            self.declare_parameter(f'{extractor}.module_name', '')
+            self.declare_parameter(f'{extractor}.class_name', '')
+            self.declare_parameter(f'{extractor}.topics', [''])
 
-            package_name = self.get_parameter(f"{extractor}.package_name").get_parameter_value().string_value
-            module_name = self.get_parameter(f"{extractor}.module_name").get_parameter_value().string_value
-            class_name = self.get_parameter(f"{extractor}.class_name").get_parameter_value().string_value
+            package_name = self._get_param(f'{extractor}.package_name').string_value
+            module_name = self._get_param(f'{extractor}.module_name').string_value
+            class_name = self._get_param(f'{extractor}.class_name').string_value
 
-            topics_name = self.get_parameter(f"{extractor}.topics").get_parameter_value().string_array_value
+            topics_name = self._get_param(f'{extractor}.topics').string_array_value
             self.topics.extend(topics_name)
             
             try:
-                module = importlib.import_module(f"{package_name}.{module_name}")
+                module = importlib.import_module(f'{package_name}.{module_name}')
                 if extractor not in self.info_extractors:
                     extractor = getattr(module, class_name)()
                     self.info_extractors[extractor.get_msg_type()] = extractor
@@ -58,7 +75,10 @@ class BagToCsvNode(Node):
                 sys.exit(1)
 
         self.convert_bag_to_csv(bag_file_path, csv_file_path)
-
+    
+    def _get_param(self, param_name):
+        return self.get_parameter(param_name).get_parameter_value()
+    
     def register_message_types(self, packages_to_register):
         for package in packages_to_register:
             try:
@@ -66,8 +86,8 @@ class BagToCsvNode(Node):
             except (PackageNotFoundError, ValueError) as exception:
                 raise exception
             self.declare_parameter(package, 'msg')
-            msgs_folder = self.get_parameter(package).get_parameter_value().string_value
-            msgs_folder_path = Path(f"{package_share_directory}/{msgs_folder}") 
+            msgs_folder = self._get_param(package).string_value
+            msgs_folder_path = Path(f'{package_share_directory}/{msgs_folder}') 
 
             for msg_file_path in msgs_folder_path.glob('*.msg'):
                 msg_name = msg_file_path.stem
@@ -94,10 +114,10 @@ class BagToCsvNode(Node):
                 topic_connections = [x for x in reader.connections if x.topic == topic_name]
                 topic_data = self._extract_data(reader, topic_connections)
                 df = pd.DataFrame(topic_data)
-                print(f"{csv_file_path}exported_{topic_name}.csv")
-                df.to_csv(f"{csv_file_path}exported_{topic_name}.csv", index=False)
+                print(f'{csv_file_path}exported_{topic_name}.csv')
+                df.to_csv(f'{csv_file_path}exported_{topic_name}.csv', index=False)
                 self.get_logger().info(f'CSV file created for {topic_name}: {csv_file_path}')
-
+    
 def main(args=None):
     rclpy.init(args=args)
     node = BagToCsvNode()
--- launch/bag_to_csv.launch.py
+++ launch/bag_to_csv.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2024 National Research Counchil STIIMA & Intelligent Robotics Lab
+# Copyright 2024 National Research Council STIIMA
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
